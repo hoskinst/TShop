e295450e4074f6122b1039b69c4d063df48f8561
Tanner <hoskinst@live.com>
2023-12-13 15:21:26 -0500

    Fixed render build bug on production

--- backend/server.js
+++ backend/server.js
@@ -23,10 +23,6 @@ app.use(express.urlencoded({ extended: true }));
 // cookie parser middleware
 app.use(cookieParser())
 
-app.get('/', (req, res) => {
-    res.send(`api is running...${process.env.NODE_ENV}`);
-})
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -47,9 +43,13 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
     // any route that is not api will be redirected to index.html
-    app.get('*', (req, res) => {
+    app.get('*', (req, res) => 
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    });
+    );
+} else {
+    app.get('/', (req, res) => {
+        res.send('api is running...');
+    })
 }
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
