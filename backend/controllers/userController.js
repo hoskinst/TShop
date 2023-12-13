62752d65faf677e521a706960487b27f9c9c061e
Tanner <hoskinst@live.com>
2023-12-13 17:30:02 -0500

    user is not defined

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,26 +19,31 @@ const authUser = asyncHandler(async(req, res) => {
 // @desc Register user
 // @route POST /api/users
 // @access public
-const registerUser = asyncHandler(async(req, res) => {
-    const { email, name, password } = req.body;
-    const userExists = await User.findOne({ email });
+const registerUser = asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email }); 
     if (userExists) {
-        res.status(400);
-        throw new Error('Email is already in use.')
+      res.status(400);
+      throw new Error('User already exists');
     }
-    
-    const user = await User.create({name, email, password});
-
+    const user = await User.create({
+      name,
+      email,
+      password,
+    });
     if (user) {
-        generateToken(res, user._id)
-        res.status(201).json({
-            _id: user._id,
-            email: user.email,
-            isAdmin: user.isAdmin,
-            name: user.name,   
-        })
+      generateToken(res, user._id);
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error('Invalid user data');
     }
-})
+  });
 
 // @desc logout user
 // @route POST /api/users/logout
