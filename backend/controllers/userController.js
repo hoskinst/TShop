a260f26e29f1453f7b9397500619652f7eaa5374
Tanner <hoskinst@live.com>
2023-12-13 18:46:47 -0500

    added missing login response

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,6 +10,12 @@ const authUser = asyncHandler(async(req, res) => {
     const user = await User.findOne({ email: email});
     if (user && (await user.matchPassword(password))) {
         generateToken(res, user._id)
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+          });
     } else {
         res.status(401)
         throw new Error('Invalid email or password');
